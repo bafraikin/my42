7345ac28eec3e3ac93e92d083f8328aad50c3d93
bafraiki <bafraiki@student.42.fr>
2018-11-06 17:32:05 +0100

    update of project "2018-11-06_17-32-05"

--- afterpis/libft/srcs/ft_memset.c
+++ afterpis/libft/srcs/ft_memset.c
@@ -1,17 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/06 14:33:39 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/11/06 17:05:07 by bafraiki         ###   ########.fr       */
+/*   Created: 2018/11/06 17:22:37 by bafraiki          #+#    #+#             */
+/*   Updated: 2018/11/06 17:26:37 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
+#include "libft.h"
 
-
+void  *ft_memset(void *b, int c, size_t len)
+{
+  while (len-- > 0)
+  {
+    *b = (unsigned char)c;
+    b++;
+  }
+}
