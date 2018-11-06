359ef0bd771e9eef8e2884414bbefc762e7c20f5
bafraiki <bafraiki@student.42.fr>
2018-11-06 19:21:34 +0100

    update of project "2018-11-06_19-21-34"

---
+++ afterpis/libft/srcs/ft_memccpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 18:42:02 by bafraiki          #+#    #+#             */
+/*   Updated: 2018/11/06 19:21:30 by bafraiki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	unsigned char *c_dst;
+	unsigned char *c_src;
+
+	while (n-- > 0)
+	{
+		c_dst = (unsigned char *)dst;
+		c_src = (unsigned char *)src;
+		if (*c_src == (unsigned char)c && dst++)
+			return (dst);
+		*c_dst = *c_src;
+		dst++;
+		src++;
+	}
+	return (NULL);
+}
