9ef68965194850d51c779037e89d5fb8e003d2eb
bafraiki <bafraiki@student.42.fr>
2018-11-12 14:26:25 +0100

    update of project "2018-11-12_14-26-25"

--- afterpis/libft/ft_strncmp.c
+++ afterpis/libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 17:32:41 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/11/07 18:55:10 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/11/12 14:26:22 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (*s1 == *s2 && *s1 != '\0' && n > 0)
+	if (n == 0)
+		return (n);
+	while (*s1 == *s2 && *s1 != '\0' && --n > 0)
 	{
 		s1++;
 		s2++;
-		n--;
 	}
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
