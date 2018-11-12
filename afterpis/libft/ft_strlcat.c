bb4320b1ad7c35ee83de5638106ddc1e7b2f2305
bafraiki <bafraiki@student.42.fr>
2018-11-12 13:56:50 +0100

    update of project "2018-11-12_13-56-50"

--- afterpis/libft/ft_strlcat.c
+++ afterpis/libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 15:38:00 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/11/07 16:26:52 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/11/12 13:56:48 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,19 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	int		i;
-	int		j;
-	size_t	val;
+	size_t len_src;
+	size_t len_dst;
 
-	val = ft_strlen(dst) + ft_strlen(src);
-	i = 0;
-	j = -1;
-	while (size-- > 0 && dst[i] != '\0')
-		i++;
-	while (size-- > 0 && src[++j] != '\0')
-	{
-		dst[i] = src[j];
-		i++;
-	}
-	if (size > 0)
-		dst[i] = '\0';
+	len_src = ft_strlen(src);
+	len_dst = ft_strlen(dst);
+	if (len_dst >= size)
+		return (size + len_src);
+	if (size - len_dst > len_src)
+		ft_memcpy(dst + len_dst, src, len_src + 1);
 	else
-		dst[i - 1] = '\0';
-	return (val);
+	{
+		ft_memcpy(dst + len_dst, src, size - 1);
+		dst[size - 1] = '\0';
+	}
+	return (len_src + len_dst);
 }
