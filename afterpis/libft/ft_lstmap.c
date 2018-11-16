fe68a16b43abf3569f84603a2805c893804af303
bafraiki <bafraiki@student.42.fr>
2018-11-16 22:55:04 +0100

    update of project "2018-11-16_22-55-04"

--- afterpis/libft/ft_lstmap.c
+++ afterpis/libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 17:27:16 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/11/16 22:18:24 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/11/16 22:54:57 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,15 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	t_list *ret;
 
 	tmp = lst;
+	new = NULL;
 	if (lst && f)
 	{
 		while (tmp)
 		{
 			ret = (*f)(tmp);
-			if ((ret = ft_lstnew(ret->content, ret->content_size)) == NULL)
-				return (NULL);
-			ft_lstadd(&new, ret);
+			ft_list_push_back(&new, ret->content, ret->content_size);
 			tmp = tmp->next;
 		}
-		ft_list_reverse(&new->next);
 		return (new);
 	}
 	return (NULL);
