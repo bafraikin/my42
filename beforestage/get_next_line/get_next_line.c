35499743f59fbb93a49ba370f598158674407124
bafraiki <bafraiki@student.42.fr>
2018-11-16 12:45:37 +0100

    update of project "2018-11-16_12-45-36"

--- beforestage/get_next_line/get_next_line.c
+++ beforestage/get_next_line/get_next_line.c
@@ -6,8 +6,13 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 12:04:12 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/11/16 12:04:15 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/11/16 12:44:14 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+int	get_next_line(const int fd,char **line)
+{
+	static t_
+
+}
--- beforestage/get_next_line/get_next_line.h
+++ beforestage/get_next_line/get_next_line.h
@@ -6,8 +6,18 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 12:04:28 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/11/16 12:04:30 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/11/16 12:45:30 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 32
+
+int	get_next_line(const int fd,char **line);
+
+typedef struct s_fdline 
+
+#endif
