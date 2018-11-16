9d03f2e5e38f5c7cb470c2dae048f578e0973d3c
bafraiki <bafraiki@student.42.fr>
2018-11-16 15:42:30 +0100

    update of project "2018-11-16_15-42-30"

--- afterpis/libft/ft_lstnew.c
+++ afterpis/libft/ft_lstnew.c
@@ -6,14 +6,25 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 14:47:54 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/11/16 14:48:52 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/11/16 15:41:31 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
+	t_list *new;
 
-
+	if ((new = (t_list*)malloc(sizeof(t_list))) == NULL)
+			return (NULL);
+	if (*content != NULL)
+	{
+		new->content = (void*)content;
+		new->content_size = content_size;
+		new->next = NULL;
+	}
+	else
+		ft_bzero(new, sizeof(t_list));
+	return (new);
 }
