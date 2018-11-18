3f2e901797587e608584cc55f9169b89960b8cdb
bafraikin <fraikin.baptiste@gmail.com>
2018-11-18 22:45:49 +0100

    tomorrow it's will be good

--- beforestage/get_next_line/get_next_line.c
+++ beforestage/get_next_line/get_next_line.c
@@ -12,19 +12,61 @@
 
 #include "get_next_line.h"
 
+static void *ft_gest_list(t_list **begin, int ret, const int fd, char *line)
+{
+  /*
+     int       ret;
+     int       total;
+     int       mode;
+     char      *t_rd->ptr_line;
+     char      *t_rd->is_read;
+     char      *tmp;
+     */
+
+
+}
+
+static t_read *ft_recup_fd(const int fd, char *line)
+{
+  t_read  *t_rd;
+
+  if (!(line && fd > 0))
+    return (NULL);
+  t_rd = (t_read*)malloc(sizeof(t_read));
+  ft_bzero(t_rd, sizeof(t_read));
+  t_rd->is_read = (char*)malloc(1);
+  if t_rd
+    while (t_rd->mode == 0 && (t_rd->ret = read(fd, *line, BUFF_SIZE)))
+    {
+      if ((t_rd->tmp = (char*)malloc(sizeof(char) * t_rd->total)) == NULL)
+        return (NULL);
+      ft_memcpy(t_rd->tmp, t_rd->is_read, t_rd->total);
+      t_rd->total += t_rd->ret;
+      free(t_rd->is_read);
+      if ((t_rd->ptr_line = ft_memchr(*line, '\n', t_rd->ret)) != NULL)
+        mode = 1;
+      if ((t_rd->is_read = (char*)malloc(sizeof(char) * t_rd->total + 1)) == NULL)
+        return (NULL);
+      ft_memcpy(t_rd->is_read, t_rd->tmp, t_rd->total - t_rd->ret);
+      free(t_rd->tmp);
+      ft_memcpy(t_rd->is_read + (t_rd->total - t_rd->ret), line, t_rd->ret);
+    }
+  t_rd->is_read[total] = '\0';
+  return (t_rd);
+}
+
 int	get_next_line(const int fd,char **line)
 {
-  static t_list *begin;
-  int ret;
+  static t_list *begin = NULL;
+  t_read  *l_read;  
 
-  ret = 0;
+  l_read = NULL;
   if (line && *line && fd > 0)
-  while (ret = read(fd, *line, BUFF_SIZE))
-  {
+    l_read = ft_recup_fd(fd, *line);
+  if (!(line && *line && fd > 0 && l_read))
+    return (-1);
 
-
-  }
-  return (ret);
+  return (1)
 }
 
 int main(int argc, char *argv[])
--- beforestage/get_next_line/get_next_line.h
+++ beforestage/get_next_line/get_next_line.h
@@ -25,9 +25,29 @@ int	get_next_line(const int fd,char **line);
 
 typedef	struct		s_list
 {
-	void			*content;
-	size_t			content_size;
-	struct s_list	*next;
+  void			*content;
+  size_t			content_size;
+  struct s_list	*next;
 }					t_list;
+
+typedef struct          s_fdlin
+{
+  char          *l_line;
+  char          *l_rest;
+  int           size_line;
+  int           size_rest;
+  int           mode;
+  int           fd;
+}                       t_fdlin;
+
+typedef struct        s_read
+{
+  int ret;
+  int total;
+  int mode;
+  char *ptr_l;
+  char *is_read;
+  char *tmp;
+}                     t_read;
 
 #endif
