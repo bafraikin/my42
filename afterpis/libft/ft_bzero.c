4fd19bdbff87a42619861808640d096a2ea2a3a1
bafraiki <bafraiki@student.42.fr>
2018-11-19 13:05:47 +0100

    norme is ok

--- afterpis/libft/ft_bzero.c
+++ afterpis/libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 18:06:07 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/11/16 22:17:17 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/11/19 13:05:26 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-  size_t i;
-  long *cl_s;
-  unsigned char *cc_s;
+	size_t			i;
+	long			*cl_s;
+	unsigned char	*cc_s;
 
-  i = 0;
-  cc_s = NULL;
-  if (n % sizeof(long) != 0)
-  {
-    cc_s = s;
-    while (n % sizeof(long) != 0 && n-- > 0)
-      *(cc_s++) = 0;
-  }
-  cl_s = (cc_s == NULL) ? (long*)s : (long*)cc_s;
-  if (n > 0)
-    while(i < n / sizeof(long))
-    {
-      *cl_s = 0;
-      cl_s++;
-      i++;
-    }
+	i = 0;
+	cc_s = NULL;
+	if (n % sizeof(long) != 0)
+	{
+		cc_s = s;
+		while (n % sizeof(long) != 0 && n-- > 0)
+			*(cc_s++) = 0;
+	}
+	cl_s = (cc_s == NULL) ? (long*)s : (long*)cc_s;
+	if (n > 0)
+		while (i < n / sizeof(long))
+		{
+			*cl_s = 0;
+			cl_s++;
+			i++;
+		}
 }
--- afterpis/libft/ft_memcpy.c
+++ afterpis/libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 18:15:47 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/11/16 22:15:36 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/11/19 13:04:35 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-  void *mem;
-  size_t i;
-  long *cl_dst;
-  long *cl_src;
+	void	*mem;
+	size_t	i;
+	long	*cl_dst;
+	long	*cl_src;
 
-  i = 0;
-  mem = dst;
-  if (n % sizeof(long) != 0)
-    while (n % sizeof(long) != 0 && n-- > 0)
-      *(unsigned char*)(dst++) = *(unsigned char*)(src++);
-  cl_dst = (long*)dst;
-  cl_src = (long*)src;
-  if (n > 0)
-    while(i < n / sizeof(long))
-    {
-      *cl_dst = *cl_src;
-      cl_src++;
-      cl_dst++;
-      i++;
-    }
-  return (mem);
+	i = 0;
+	mem = dst;
+	if (n % sizeof(long) != 0)
+		while (n % sizeof(long) != 0 && n-- > 0)
+			*(unsigned char*)(dst++) = *(unsigned char*)(src++);
+	cl_dst = (long*)dst;
+	cl_src = (long*)src;
+	if (n > 0)
+		while (i < n / sizeof(long))
+		{
+			*cl_dst = *cl_src;
+			cl_src++;
+			cl_dst++;
+			i++;
+		}
+	return (mem);
 }
