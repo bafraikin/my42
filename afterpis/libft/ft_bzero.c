29a75448057b10c6799e84be81e2ae11c3d87d29
bafraiki <bafraiki@student.42.fr>
2018-11-19 18:33:37 +0100

    need ti try ft_bzero

--- afterpis/libft/ft_bzero.c
+++ afterpis/libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 18:06:07 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/11/19 13:05:26 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/11/19 18:33:04 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,17 @@ void	ft_bzero(void *s, size_t n)
 	long			*cl_s;
 	unsigned char	*cc_s;
 
-	i = 0;
-	cc_s = NULL;
-	if (n % sizeof(long) != 0)
+	i = 1;
+	cl_s = NULL;
+	if ((unsigned int)s % sizeof(long) == 0)
 	{
-		cc_s = s;
-		while (n % sizeof(long) != 0 && n-- > 0)
-			*(cc_s++) = 0;
+		cl_s = s;
+		while (n / (i * 8) >= 1)
+			*(cl_s++) = 0;
+		n = n - (i * 8);
 	}
-	cl_s = (cc_s == NULL) ? (long*)s : (long*)cc_s;
+	cc_s = (cl_s == NULL) ? (unsigned char*)s : (unsigned char*)cl_s;
 	if (n > 0)
-		while (i < n / sizeof(long))
-		{
-			*cl_s = 0;
-			cl_s++;
-			i++;
-		}
+		while (n-- > 0)
+			*(cc_s++) = 0;
 }
