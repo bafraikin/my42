f45d41b045db36ace5edc15e1f968df2629a7292
bafraiki <bafraiki@student.42.fr>
2018-12-17 17:06:36 +0100

    update of project "2018-12-17_17-06-36"

--- beforestage/fillit/src/check.c
+++ beforestage/fillit/src/check.c
@@ -6,11 +6,25 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/17 15:16:36 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/12/17 16:29:55 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/12/17 17:06:08 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+int		ft_strlen_strchr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != '#' || str[i] != '.')
+			exit(EXIT_FAILURE);
+		i++;
+	}
+	return (i);
+}
 
 void	ft_store_grid(int fd ,char **grid)
 {
@@ -31,11 +45,27 @@ int	ft_grid_validity(int fd)
 {
 	char *grid[130];
 	int i;
+	int nb_line;
+	int nb_hash;
 
-	i = -1;
+	i = 0;
 	ft_store_grid(fd, grid);
-	while (grid[++i])
-		printf("%s\n", grid[i]);
+
+	while (grid[i])
+	{
+		nb_line = 0;
+		while (grid[i] && *grid[i] != '\n' && nb_line < 4)
+		{
+			if (ft_strlen_strchr(grid[i]) != 4)
+				exit (EXIT_FAILURE);
+			nb_line++;
+			i++;
+		}
+		if (grid[i] && nb_line != 4)
+			exit(EXIT_FAILURE);
+		i++;
+	}
+	exit(EXIT_SUCCESS);
 	return (0);
 }
 
