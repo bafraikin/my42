c70085ef6018c0e02814aa4a711aa1b412930154
bafraiki <bafraiki@student.42.fr>
2018-12-19 19:29:47 +0100

    update of project "2018-12-19_19-29-47"

--- beforestage/fillit/src/check.c
+++ beforestage/fillit/src/check.c
@@ -6,7 +6,7 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/17 15:16:36 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/12/19 14:22:25 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/12/19 19:21:08 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,75 @@ int		ft_strlen_strchr(char *str, int *count)
 		i++;
 	}
 	return (i);
+}
+
+int		follow_piece(char form[4][2], int i)
+{
+	int j;
+	int tmp[4];
+
+	j = -1;
+	while (++j < 4)
+		tmp[j] = form[j][i];
+	j = -1;
+	while (++j < 3)
+		if (tmp[j] > tmp[j + 1])
+		{
+			ft_swap(&tmp[j], &tmp[j + 1]);
+			j = -1;
+		}
+	j = -1;
+	while (++j < 3)
+		if (tmp[j + 1] - tmp[j] > 1)
+			exit (EXIT_FAILURE);
+	return (1);
+}
+
+int		adjacent_piece(char tab[4][2])
+{
+	int i;
+	int j;
+	int min;
+	int t;
+
+//	printf("%d %d\n%d %d\n%d %d\n%d %d\n\n", tab[0][0], tab[0][1], tab[1][0], tab[1][1], tab[2][0], tab[2][1], tab[3][0], tab[3][1]);
+	i = 0;
+	while (i < 4)
+	{
+		j = -1;
+		min = 2;
+		while (++j < 4)
+			if (j != i)
+			{
+				t = ft_cmp(tab[i][0],tab[j][0]) + ft_cmp(tab[i][1], tab[j][1]);
+				if (t < min)
+					min = t;
+			}
+		if (min > 1)
+			exit(EXIT_FAILURE);
+		i++;
+	}
+	return (1);
+}
+
+void	ft_check_fill(char **grid, char form[4][2])
+{
+	int		i;
+	int		j;
+	int 	k;
+	int		lm[2];
+
+	i = -1;
+	lm[0] = 5;
+	lm[1] = 0;
+	k = 0;
+	ft_add_value(form, 0, 0, k++);
+	while(++i < 4 && (j = -1) < 0)
+		while (++j < 4)
+			if (grid[i][j] == '#' && lm[0] == 5 && (lm[0] = i) >= 0)
+				lm[1] = j;
+			else if (grid[i][j] == '#')
+				ft_add_value(form, i - lm[0], j - lm[1], k++);
 }
 
 void	ft_store_grid(int fd ,char **grid)
@@ -52,6 +121,7 @@ int	ft_grid_validity(int fd)
 	int i;
 	int nb_line;
 	int nb_hash;
+	char form[4][2];
 
 	i = 0;
 	ft_store_grid(fd, grid);
@@ -62,6 +132,9 @@ int	ft_grid_validity(int fd)
 				exit (EXIT_FAILURE);
 		if (nb_hash != 4 || nb_line != 4
 				|| (grid[i] && ((!grid[i + 1]) || (*grid[i] != '\0'))))
+			exit(EXIT_FAILURE);
+		ft_check_fill(&grid[i - 4], form);
+		if (!((follow_piece(form, 0) && follow_piece(form, 1) && adjacent_piece(form))))
 			exit(EXIT_FAILURE);
 		if ((nb_hash = 0) == 0 && grid[i])
 			i++;
--- beforestage/fillit/src/utils.c
+++ beforestage/fillit/src/utils.c
@@ -1,33 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fillit.h                                           :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/17 15:14:36 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/12/19 19:10:13 by bafraiki         ###   ########.fr       */
+/*   Created: 2018/12/19 16:32:22 by bafraiki          #+#    #+#             */
+/*   Updated: 2018/12/19 19:14:55 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FILLIT_H
-# define FILLIT_H
-# include <fcntl.h>
-# include <unistd.h>
-# include <stdio.h>
-# include "get_next_line.h"
+void	ft_add_value(char form[4][2], int i, int j, int k)
+{
+	form[k][0] = i;
+	form[k][1] = j;
+}
 
-int				ft_grid_validity(int fd);
-void			ft_add_value(char form[4][2], int i, int j, int k);
-int				ft_cmp(int a, int b);
-void			ft_swap(int *a, int *b);
+int	ft_cmp(int a, int b)
+{
+	if (b < a)
+		return (a - b);
+	else
+		return (b - a);
+}
 
-typedef struct	s_shape {
+void	ft_swap(int *a, int *b)
+{
+	int c;
 
-	char		form[4][2];
-	char		height;
-	char		width;
-	char		letter;
-}				t_shape;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
 
-#endif
+
