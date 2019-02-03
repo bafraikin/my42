529218d459c325bfbf91c9696564c5d304be49df
bafraikin <fraikin.baptiste@gmail.com>
2019-02-03 23:29:26 +0100

    go on my project

---
+++ beforestage/roger-skyline1/html/script/main.js
@@ -0,0 +1,15 @@
+
+
+let googleVar = document.getElementsByClassName('hide');
+let Mangetesmort = document.getElementById("magic");
+
+Mangetesmort.onclick = function() {
+document.getElementsByClassName("hide")[0].style.display = "block" ;
+Mangetesmort.style.display = 'none' ;
+}
+
+
+
+
+
+
