eafcbdacfa5a6719fa8b708a9677d29b73d1ffde
bafraiki <bafraiki@student.42.fr>
2019-02-05 16:40:36 +0100

    update of project "2019-02-05_16-40-36"

--- beforestage/roger-skyline1/deploy/crontab_dif.sh
+++ beforestage/roger-skyline1/deploy/crontab_dif.sh
@@ -4,6 +4,6 @@ cp /etc/crontab /root/to_check
 diff /root/past_cron /root/to_check > /dev/null 2>&1
 if [ $? = 1 ]
 then
-	cat to_check | mail -s "diff found in crontab" root@localhost
+	cat to_check | mail -s "diff found in crontab" fraikin.baptiste@gmail.com
 fi
 mv /root/to_check /root/past_cron
