6385daeeda657c62a2aeb4b983bb90cb7ca02f5c
bafraiki <bafraiki@student.42.fr>
2019-02-06 14:20:11 +0100

    update of project "2019-02-06_14-20-11"

--- beforestage/FdF/fdf.c
+++ beforestage/FdF/fdf.c
@@ -6,14 +6,16 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 14:59:42 by bafraiki          #+#    #+#             */
-/*   Updated: 2019/02/05 17:50:58 by bafraiki         ###   ########.fr       */
+/*   Updated: 2019/02/06 14:17:23 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
+#include "fdf.h"
 #include <stdlib.h>
 #include <math.h>
 
+#include <unistd.h>
 
 
 int	ft_x(int x, int y)
@@ -27,82 +29,68 @@ int	ft_y(int x, int y)
 }
 
 
-
+int	tarace(void *param)
+{
+	int i;
+	t_fdf *win = (t_fdf*)param;
+	usleep(100000);
+	printf("%d\n", win->i);
+	write(1, "coucou\n", 7);
+	mlx_pixel_put(win->mlx_ptr, win->win_ptr, win->x + win->i++, win->y + 100, 2552111);
+	return (1);
+}
 
 
 int main(int argc, char *argv[])
 {
 
-	void *mlx_ptr;
-	void *win_ptr;
+	t_fdf win;
 	float i;
 	int x;
 	int y;
-	int angle;
-	int width;
 
-
-	angle = 1;
-	width = 200;
-	x = 500;
-	y = 200;
+	win.width = 200;
+	win.x = 500;
+	win.y = 200;
 	i = -1;
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, "coucou");
-	while (++i < width)
+	win.mlx_ptr = mlx_init();
+	win.win_ptr = mlx_new_window(win.mlx_ptr, 1000, 1000, "coucou");
+	while (++i < win.width)
 	{
-
 		/*
-		   mlx_pixel_put(mlx_ptr, win_ptr, i - place, (i + place) / 2, 2551000);
-		   mlx_pixel_put(mlx_ptr, win_ptr, i - (place + width), (i + place + width) / 2, 2551000);
-		   mlx_pixel_put(mlx_ptr, win_ptr, place - i, (i + place) / 2, 2551000);
-		   mlx_pixel_put(mlx_ptr, win_ptr, (place + width - i), (i + place + width) / 2, 2551000);
+		   mlx_pixel_put(win.mlx_ptr, win.win_ptr, i - place, (i + place) / 2, 2551000);
+		   mlx_pixel_put(win.mlx_ptr, win.win_ptr, i - (place + win.width), (i + place + win.width) / 2, 2551000);
+		   mlx_pixel_put(win.mlx_ptr, win.win_ptr, place - i, (i + place) / 2, 2551000);
+		   mlx_pixel_put(win.mlx_ptr, win.win_ptr, (place + win.width - i), (i + place + win.width) / 2, 2551000);
 		   */
-		mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y), ft_y(x + i, y), 2551000);
-		mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + width), ft_y(x + i, y + width), 2551000);
-		mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x, y + i), ft_y(x, y + i), 2551000);
-		mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + width, y + i) , ft_y(x + width, y + i), 2551000);
-		if (i < width / 2)
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + i / 2, 2551000);
+		mlx_pixel_put(win.mlx_ptr, win.win_ptr, ft_x(x + i, y), ft_y(x + i, y), 2551000);
+		mlx_pixel_put(win.mlx_ptr, win.win_ptr, ft_x(x + i, y + win.width), ft_y(x + i, y + win.width), 2551000);
+		mlx_pixel_put(win.mlx_ptr, win.win_ptr, ft_x(x, y + i), ft_y(x, y + i), 2551000);
+		mlx_pixel_put(win.mlx_ptr, win.win_ptr, ft_x(x + win.width, y + i) , ft_y(x + win.width, y + i), 2551000);
+		if (i < win.width / 2)
+			mlx_pixel_put(win.mlx_ptr, win.win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + i / 2, 2551000);
 		else
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (((width / 2) / 2) - ((i - width / 2) / 2)), 2551000);
-		if (i < width / 2)
+			mlx_pixel_put(win.mlx_ptr, win.win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (((win.width / 2) / 2) - ((i - win.width / 2) / 2)), 2551000);
+		if (i < win.width / 2)
 		{
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 12/6), 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 12/6) + 1, 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 12/7), 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 12/7) + 1, 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 12/8), 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 12/8) + 1, 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 12/9), 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 12/9) + 1, 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 12/10), 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 12/10) + 1, 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 12/11), 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 12/11) + 1, 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 13/6), 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 13/6) + 1, 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 14/6), 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 14/6) + 1, 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 15/6), 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 15/6) + 1, 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 17/6) + 1, 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 17/6) + 1, 2551000);
+			mlx_pixel_put(win.mlx_ptr, win.win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 2) + 1, 2551000);
+			mlx_pixel_put(win.mlx_ptr, win.win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (i * 2), 2551000);
 		}
 		else
 		{
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (((width / 2) * (12/6)) - ((i - width / 2) * (12/6))), 2551000);
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), (ft_y(x + i, y + 100) + (((width / 2) * (12/6)) - ((i - width / 2) * (12/6)))) + 1, 2551000);
+			mlx_pixel_put(win.mlx_ptr, win.win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) + (((win.width / 2) * (12/6)) - ((i - win.width / 2) * (12/6))), 2551000);
+			mlx_pixel_put(win.mlx_ptr, win.win_ptr, ft_x(x + i, y + 100), (ft_y(x + i, y + 100) + (((win.width / 2) * (12/6)) - ((i - win.width / 2) * (12/6)))) + 1, 2551000);
 		}
-		if (i < width / 2)
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) - i *  2, 2551000);
+		if (i < win.width / 2)
+			mlx_pixel_put(win.mlx_ptr, win.win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) - i *  2, 2551000);
 		else
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) - (((width / 2) * 2) - ((i - width / 2) * 2)), 2551000);
-		if (i < width / 2)
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) - i / 2, 2551000);
+			mlx_pixel_put(win.mlx_ptr, win.win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) - (((win.width / 2) * 2) - ((i - win.width / 2) * 2)), 2551000);
+		if (i < win.width / 2)
+			mlx_pixel_put(win.mlx_ptr, win.win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) - i / 2, 2551000);
 		else
-			mlx_pixel_put(mlx_ptr, win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) - (((width / 2) / 2 ) - ((i - width / 2) / 2)), 2551000);
+			mlx_pixel_put(win.mlx_ptr, win.win_ptr, ft_x(x + i, y + 100), ft_y(x + i, y + 100) - (((win.width / 2) / 2 ) - ((i - win.width / 2) / 2)), 2551000);
 	}
-	mlx_loop(mlx_ptr);
-	return 0;
+	mlx_loop_hook(win.mlx_ptr, tarace, (void*)&win);
+	mlx_loop(win.mlx_ptr);
+	return (0);
 }
---
+++ beforestage/FdF/fdf.h
@@ -0,0 +1,8 @@
+typedef struct s_fdf
+{
+	void *mlx_ptr;
+	void *win_ptr;
+	int i;
+	int x;
+	int y;
+} t_fdf;
