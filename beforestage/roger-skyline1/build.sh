9e3ace67f99a378a63b34883e3d88e2f19ba5c7f
bafraiki <bafraiki@student.42.fr>
2019-02-08 20:15:03 +0100

    update of project "2019-02-08_20-15-03"

--- beforestage/roger-skyline1/build.sh
+++ beforestage/roger-skyline1/build.sh
@@ -15,7 +15,7 @@ vbox_name=$vbox/$name
 iso=/sgoinfre/goinfre/ISO/Debian/debian-9.6.0-i386-DVD-1.iso
 VBoxManage createvm --name "$name" --ostype Debian_64 --register
 VBoxManage modifyvm "$name" --memory 1024 --acpi on --nic1 bridged --nictype1 82540EM --bridgeadapter1 en0 --vram 160
-VBoxManage createhd --filename "$vbox_name/$name.vdi" --size 8000
+VBoxManage createhd --filename "$vbox_name/$name.vdi" --size 7700
 VBoxManage storagectl $name --name "SATA Controller" --add sata --controller IntelAHCI
 VBoxManage storageattach $name --storagectl "SATA Controller" --port 0 --device 0 \
 	--type hdd --medium "$vbox_name/$name.vdi"
