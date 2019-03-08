3174a3ada3dbeaf5197b835bcd8741bc788af9ee
bafraiki <bafraiki@student.42.fr>
2019-03-08 18:47:51 +0100

    update of project "2019-03-08_18-47-51"

--- beforestage/FdF/fdf.c
+++ beforestage/FdF/fdf.c
@@ -6,12 +6,13 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 14:59:42 by bafraiki          #+#    #+#             */
-/*   Updated: 2019/03/07 21:52:18 by bafraiki         ###   ########.fr       */
+/*   Updated: 2019/03/08 18:41:27 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 #include "fdf.h"
+#include "libft.h"
 
 int	ft_x(int *rendu, int x, int y)
 {
@@ -46,21 +47,16 @@ t_pnt	*ft_fill_pnt(t_pnt *to_fill, int y, int x, int z)
 
 int	ft_multiply(int y, int x)
 {
-
-	printf("multiply %dy * %dx\n", y, x);
 	return (y * x);
 }
 
 int	ft_divide(int y, int x)
 {
-	printf("divide %d / %d\n", y, x);
 	return (y / x);
 }
 
 int pythagore(int x, int y)
 {
-
-
 	return (round(sqrt(pow(x, 2) + pow(y, 2))));
 }
 
@@ -71,7 +67,7 @@ void ft_cal_pnt(t_pnt *to_calc, int (*f)(int x, int y), int nb)
 	to_calc->z = (*f)(to_calc->z, nb);
 }
 
-void ft_makeline(t_img *img, t_pnt *pnt1, t_pnt *pnt2)
+void ft_makeline(t_img *img, t_pars *pars, t_pnt *pnt1, t_pnt *pnt2)
 {
 	int		i;
 	t_pnt	diff;
@@ -83,6 +79,7 @@ void ft_makeline(t_img *img, t_pnt *pnt1, t_pnt *pnt2)
 	diff.x = (pnt2->x - pnt1->x);
 	diff.y = (pnt2->y - pnt1->y);
 	ratio = diff.z != 0 ? pythagore(diff.x, diff.y) : img->pbc;
+	img->f = img->f == 0 ? ratio / 2 : img->f;
 	ft_cal_pnt(&diff, &ft_multiply, img->prec);
 	ft_cal_pnt(&diff, &ft_divide, ratio * img->pbc);
 	ft_cal_pnt(pnt1, &ft_multiply, img->prec);
@@ -92,10 +89,10 @@ void ft_makeline(t_img *img, t_pnt *pnt1, t_pnt *pnt2)
 		pnt1->y += diff.y;
 		pnt1->z += diff.z;
 		ft_y(&xy[1], ft_x(&xy[0], pnt1->x, pnt1->y), pnt1->y);
-		if (img->endian)
-			ft_color_it(img, pnt1->x / img->prec, pnt1->y / img->prec + (pnt1->z / img->prec) / (ratio / 4) * -1  ,  255 + pnt1->z / pow(img->prec, 2));
+		if (img->angle)
+			ft_color_it(img, pnt1->x / img->prec + img->d_x, pnt1->y / img->prec + img->d_y + (pnt1->z / img->prec) / (img->f) * -1  ,  255 + pnt1->z * img->f);
 		else
-			ft_color_it(img, xy[0] / img->prec + img->pbc * 11 , xy[1] / img->prec + (pnt1->z / img->prec) / (ratio / 4) * -1  ,  255 + pnt1->z / pow(img->prec, 2));
+			ft_color_it(img, xy[0] / img->prec + img->d_x , xy[1] / img->prec + (pnt1->z / img->prec) / (img->f) * -1 + img->d_y  ,  255 + pnt1->z * img->f);
 	}
 }
 
@@ -111,54 +108,99 @@ void	ft_draw_me_a_sheep(t_pars *pars, t_img *img)
 		{	
 			ft_cal_pnt(ft_fill_pnt(&pnt[0], i, j, pars->map[i][j]), &ft_multiply, img->pbc);
 			ft_cal_pnt(ft_fill_pnt(&pnt[1], i, j + 1, pars->map[i][j + 1]), &ft_multiply, img->pbc);
-			ft_makeline(img, &pnt[0], &pnt[1]);
+			ft_makeline(img, pars, &pnt[0], &pnt[1]);
 			if (i < pars->nb_l - 1)
 			{
 				ft_cal_pnt(ft_fill_pnt(&pnt[0], i, j, pars->map[i][j]), &ft_multiply, img->pbc);
 				ft_cal_pnt(ft_fill_pnt(&pnt[1], i + 1, j, pars->map[i + 1][j]), &ft_multiply, img->pbc);
-				ft_makeline(img, &pnt[0], &pnt[1]);
+				ft_makeline(img, pars, &pnt[0], &pnt[1]);
 				if (j == pars->size_l - 2)
 				{
 					ft_cal_pnt(ft_fill_pnt(&pnt[0], i, j + 1, pars->map[i][j + 1]), &ft_multiply, img->pbc);
 					ft_cal_pnt(ft_fill_pnt(&pnt[1], i + 1, j + 1, pars->map[i + 1][j + 1]), &ft_multiply, img->pbc);
-					ft_makeline(img, &pnt[0], &pnt[1]);
+					ft_makeline(img, pars, &pnt[0], &pnt[1]);
 				}
 			}
 		}
+}
+
+void	free_parsing(t_pars *pars)
+{
+	int i;
+
+	i = 0;
+	while (pars->map[i])
+		free(pars->map[i++]);
+	free(pars->map);
+}
+
+void generate_win(t_mlx *mlx)
+{
+	if (mlx->img.ptr != NULL)
+	{
+		mlx_destroy_image(mlx->ptr, mlx->img.ptr);
+		mlx_clear_window(mlx->ptr, mlx->win);
+	}
+	mlx->img.ptr = mlx_new_image(mlx->ptr, mlx->img.size, mlx->img.size);
+	mlx->img.data = mlx_get_data_addr(mlx->img.ptr, &(mlx->img.bbp), &(mlx->img.size_line), &(mlx->img.endian));
+	ft_draw_me_a_sheep(mlx->pars, &(mlx->img));
+	mlx_put_image_to_window(mlx->ptr, mlx->win, mlx->img.ptr, 0, 0);
+}
+
+int key_hook(int keycode, void *params)
+{
+	t_mlx *mlx;
+
+	mlx = (t_mlx*)params;
+	if (keycode == 53)
+	{
+		mlx_destroy_image(mlx->ptr, mlx->img.ptr);
+		free_parsing(mlx->pars);
+		mlx_clear_window (mlx->ptr, mlx->win);
+		mlx_destroy_window(mlx->ptr, mlx->win);
+		exit(EXIT_SUCCESS);
+	}
+	else if (keycode == 49)
+		mlx->img.angle = (mlx->img.angle) ? 0 : 1;
+	else if(keycode == 126)
+		mlx->img.pbc += 1;
+	else if(keycode == 125 && mlx->img.pbc - 1 > 0)
+		mlx->img.pbc -= 1;
+	else if(keycode == 123)
+		mlx->img.d_x -= mlx->pars->nb_l;
+	else if(keycode == 124)
+		mlx->img.d_x += mlx->pars->nb_l;
+	else if(keycode == 69)
+		mlx->img.f--; 
+	else if(keycode == 78)
+		mlx->img.f++; 
+	generate_win(mlx);
+	return (0);
 }
 
 int main(int argc, char *argv[])
 {
 	t_pars	*pars;
 	t_mlx	mlx;
-	t_img	img;
 	int fd;
 
 	if (argc != 2)
 		exit(EXIT_FAILURE);
 	if (!(fd = open(argv[1], O_RDONLY)))
 		exit(EXIT_FAILURE);
+	ft_bzero(&mlx, sizeof(t_mlx));
 	pars = ft_parse_map(fd);
-
-	img.prec = 10000;
-	img.pbc = (pars->nb_l > pars->size_l) ? 900 / pars->nb_l / 2 :  1250 / pars->size_l / 2 ;
-	img.size = (pars->nb_l > pars->size_l) ? 1500 : 1250;
+	mlx.pars = pars;
+	mlx.img.prec = 10000;
+	mlx.img.angle = 1;
+	mlx.img.pbc = (pars->nb_l > pars->size_l) ? 900 / pars->nb_l / 2 :  1250 / pars->size_l / 2 ;
+	mlx.img.d_x = mlx.img.pbc * pars->nb_l;
+	mlx.img.d_y = pars->nb_l * mlx.img.pbc;
+	mlx.img.size = (pars->nb_l > pars->size_l) ? 1500 : 1250;
 	mlx.ptr = mlx_init();
-	mlx.win = mlx_new_window(mlx.ptr, 1500, 1250, "coucou");
-
-	img.ptr = mlx_new_image(mlx.ptr, img.size, img.size);
-	img.data = mlx_get_data_addr(img.ptr, &(img.bbp), &(img.size_line), &(img.endian));
-
-
-	printf("ici\n");
-	ft_draw_me_a_sheep(pars, &img);
-	printf("ici\n");
-	mlx_put_image_to_window(mlx.ptr, mlx.win, img.ptr, 80, 80);
+	mlx.win = mlx_new_window(mlx.ptr, 1500, 1250, "FDF");
+	generate_win(&mlx);
+	mlx_key_hook(mlx.win, &key_hook, &mlx);
 	mlx_loop(mlx.ptr);
-
-	fd = 0;
-	while (pars->map[fd])
-		free(pars->map[fd++]);
-	free(pars->map);
 	return (0);
 }
--- beforestage/FdF/includes/fdf.h
+++ beforestage/FdF/includes/fdf.h
@@ -1,20 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/08 14:17:46 by bafraiki          #+#    #+#             */
+/*   Updated: 2019/03/08 18:14:20 by bafraiki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H 
+
+# include <stdlib.h>
+# include <math.h>
+# include <unistd.h>
+# include <fcntl.h>
 
 
-#include <stdlib.h>
-#include <math.h>
-#include <unistd.h>
-#include <fcntl.h>
-
-
-typedef struct s_mlx
+typedef struct	s_pars
 {
-	void *ptr;
-	void *win;
-	int height;
-	int width;
-} t_mlx;
+	short **map;
+	short **tmp;
+	int size_l;
+	int nb_l;
+}				t_pars;
 
-typedef struct s_img
+typedef struct	s_img
 {
 	int size;
 	void *ptr;
@@ -24,23 +37,32 @@ typedef struct s_img
 	short pbc;
 	int prec;
 	int endian;
-} t_img;
+	int d_x;
+	int f;
+	int d_y;
+	int angle;
+}				t_img;
 
-typedef struct s_pars
+typedef struct	s_mlx
 {
-	short **map;
-	short **tmp;
-	int size_l;
-	int nb_l;
-} t_pars;
+	void *ptr;
+	void *win;
+	int height;
+	int width;
+	t_img img;
+	t_pars *pars;
+}				t_mlx;
 
-typedef struct s_pnt
+
+typedef struct	s_pnt
 {
 	int x;
 	int y;
 	int z;
-} t_pnt;
+}				t_pnt;
 
 
 
 t_pars *ft_parse_map(int fd);
+
+#endif /* ifndef FDF_H */
