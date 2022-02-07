e37640f38c8bacf45aaf8397bdfaf68866ef0966
Baptiste FRAIKIN <bafraiki@e2r3p7.clusters.42paris.fr>
2022-02-07 12:03:58 +0100

    finished colleen

--- after/quine/ASM/Colleen.s
+++ after/quine/ASM/Colleen.s
@@ -17,7 +17,8 @@ mov rdx, 10
 mov rcx, 34
 xor rax, rax
 call printf
+xor rax, rax
 ret
 
 message:
-db "global main%2$cextern printf%2$c%2$csection .text%2$c%2$c;coucou Colleen%2$csalut:%2$cmov rdi, 0%2$cinc rdi%2$cret%2$c%2$cmain:%2$ccall salut%2$cmov rdi, message ; salut%2$cmov rsi, message%2$cmov rdx, 10%2$cmov rcx, 34%2$cxor rax, rax%2$ccall printf%2$cret%2$c%2$cmessage:%2$cdb %3$c%1$s%3$c, 0%2$c", 0
+db "global main%2$cextern printf%2$c%2$csection .text%2$c%2$c;coucou Colleen%2$csalut:%2$cmov rdi, 0%2$cinc rdi%2$cret%2$c%2$cmain:%2$ccall salut%2$cmov rdi, message ; salut%2$cmov rsi, message%2$cmov rdx, 10%2$cmov rcx, 34%2$cxor rax, rax%2$ccall printf%2$cxor rax, rax%2$cret%2$c%2$cmessage:%2$cdb %3$c%1$s%3$c, 0%2$c", 0
